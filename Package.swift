f2ca90803984ea4a93ba9bd94d9a77bf4f53326f
Jonas Nordstrand <jonas.nordstrand@sparebank1.no>
2025-01-16 13:38:32 +0100

    bump iOS version

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CxxLibrary",
-    platforms: [.macOS(.v13), .iOS(.v15)],
+    platforms: [.macOS(.v13), .iOS(.v16)],
     products: [
         .library(
             name: "CxxLibrary",
